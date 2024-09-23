ee5d09cd409e20c5d8e09ff913e9ccffa15d152a
Zyztem <heyitzyztem@outlook.com>
2024-09-23 16:35:41 -0400

    Add Better SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -22,10 +22,23 @@ const config: DocsThemeConfig = {
   primaryHue: 264,
   head: (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="NodeByte Knowledgebase" />
-      <meta property="og:description" content="Welcome to the **NodeByte Hosting Knowledge Base**! Explore guides, troubleshooting tips, and best practices to make the most of our hosting services. Whether you're just starting or need advanced help, we've got the resources you need." />
-      <link rel="icon" type="image/png" href="https://beta.nodebyte.host/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo-Black.34859308.png&w=96&q=75"></link>
+      <meta name="description" content="Welcome to the NodeByte Hosting KnowledgeBase! Explore guides, troubleshooting tips, and best practices to make the most of our hosting services. Whether you're just starting or need advanced help, we've got the resources you need." />
+      <meta name="keywords" content="NodeByte, hosting, knowledge base, guides, troubleshooting, best practices" />
+      <meta name="author" content="NodeByte Hosting" />
+      <meta name="robots" content="index, follow" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="%s - NodeByte Knowledgebase" />
+      <meta property="og:description" content="Welcome to the NodeByte Hosting KnowledgeBase! Explore guides, troubleshooting tips, and best practices to make the most of our hosting services." />
+      <meta property="og:image" content="https://beta.nodebyte.host/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo-Black.34859308.png&w=96&q=75" />
+      <meta property="og:url" content="https://beta.nodebyte.host/" />
+      <meta property="og:site_name" content="NodeByte Hosting" />
+      <meta property="og:author" content="NodeByte Hosting" />
+      <meta property="twitter:image" content="https://beta.nodebyte.host/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo-Black.34859308.png&w=96&q=75" />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:title" content="%s - NodeByte Knowledgebase" />
+      <meta property="twitter:description" content="Welcome to the NodeByte Hosting KnowledgeBase! Explore guides, troubleshooting tips, and best practices to make the most of our hosting services." />
+      <meta property="twitter:site" content="@YourTwitterHandle" />
+      <link rel="icon" type="image/png" href="https://beta.nodebyte.host/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo-Black.34859308.png&w=96&q=75" />
     </>
   ),
  banner: {
